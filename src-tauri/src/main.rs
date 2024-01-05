87fc74d5b4f195fd4cc37fa4dc1683baaef09f7c
eatradish <sakiiily@aosc.io>
2024-01-05 23:57:17 +0800

    init

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,31 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::{
+    fs::File,
+    io::{self, Write}, process::exit,
+};
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn set_locale(locale: &str) {
+    if let Err(e) = set_locale_inner(locale) {
+        eprintln!("{e}");
+    }
+    exit(0);
+}
+
+fn set_locale_inner(locale: &str) -> io::Result<()> {
+    let mut f = File::create("/etc/locale.conf")?;
+    f.write_all(b"LANG=")?;
+    f.write_all(format!("{locale}\n").as_bytes())?;
+
+    Ok(())
+}
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![set_locale])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
---
+++ vite.config.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+import { fileURLToPath, URL } from "url";
+
+import path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueI18n from "@intlify/vite-plugin-vue-i18n";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueI18n({ include: path.resolve(__dirname, "./src/locales/**") }),
+  ],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
